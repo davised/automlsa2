da578138e14164b1eb97122a4d4b296aeefaf961
Edward Davis <ed@cgrb.oregonstate.edu>
2020-11-06 15:36:28 -0800

    Add tracking for BLAST searches.

--- automlsa2/blast_functions.py
+++ automlsa2/blast_functions.py
@@ -264,7 +264,7 @@ def print_fasta_files(blastout: pd.DataFrame, labels: List[str]) -> List[str]:
         os.mkdir(fastdir)
     msg = 'Writing unaligned FASTA sequences, if necessary.'
     logger.info(msg)
-    for name, group in blastout.groupby('qseqid'):
+    for name, group in track(blastout.groupby('qseqid')):
         fasta: str = os.path.join(fastdir, '{}.fas'.format(name))
         unaligned.append(fasta)
         if os.path.exists(fasta):
