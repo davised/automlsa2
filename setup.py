80e9babfe11e1106597dbfc3ce785f7b6ff06f72
Edward Davis <ed@cgrb.oregonstate.edu>
2020-11-21 00:06:47 -0800

    Format with Black.

--- setup.py
+++ setup.py
@@ -11,10 +11,7 @@ with open('automlsa2/__version__.py', 'r') as f:
 with open('README.rst', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-REQUIRES = ['biopython',
-            'pandas>=1.0.0',
-            'numpy',
-            'rich']
+REQUIRES = ['biopython', 'pandas>=1.0.0', 'numpy', 'rich']
 
 kwargs = {
     'name': 'automlsa2',
@@ -43,11 +40,8 @@ kwargs = {
     'install_requires': REQUIRES,
     'tests_require': ['coverage', 'pytest'],
     'packages': find_packages(exclude=('tests', 'tests.*')),
-
 }
 
-setup(**kwargs,
-      entry_points={
-          "console_scripts": ['automlsa2 = automlsa2.__main__:main']
-      }
-      )
+setup(
+    **kwargs, entry_points={"console_scripts": ['automlsa2 = automlsa2.__main__:main']}
+)
